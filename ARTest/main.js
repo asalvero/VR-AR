54dde5370dfa86e950095a0f8a337563546c1aab
asalvero <asalvero@ursinus.edu>
2021-05-02 18:39:25 -0400

    Update main.js

--- ARTest/main.js
+++ ARTest/main.js
@@ -199,7 +199,7 @@ arToolkitContext.init();// function onCompleted(){
 //});
 
 // Adding the AR markerControls
-const patterns = ["data/hiro.patt", "data/kanji.patt", "letterA.patt", "letterB.patt", "letterC.patt", "letterD.patt", "letterF.patt", "letterG.patt"];
+const patterns = ["data/hiro.patt", "data/kanji.patt", "data/letterA.patt", "data/letterB.patt", "data/letterC.patt", "data/letterD.patt", "data/letterF.patt", "data/letterG.patt"];
 let markerRoots = [];
 let markersVisible = [];
 for (let i = 0; i < patterns.length; i++) {
